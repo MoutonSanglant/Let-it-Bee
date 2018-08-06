c6e9428c55ec48e29872817f416a593e5614c452
MoutonSanglant <defresne.thierry@gmail.com>
2018-08-06 21:56:36 +0200

    Animation blend tree, animation controls, jump & fall animations

---
+++ Assets/Scripts/CharacterController/Animation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+	private Rigidbody2D Rigidbody;
+	private Animator Animator;
+
+	private void Start()
+	{
+		Rigidbody = GetComponent<Rigidbody2D>();
+		Animator = GetComponentInChildren<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		Animator.SetFloat("SpeedX", Rigidbody.velocity.x);
+		Animator.SetFloat("SpeedY", Rigidbody.velocity.y);
+		//Debug.Log(Rigidbody.velocity.x);
+	}
+}
